536e47e26f9fd19d053e163914df9882352be716
duog1 <duong39991@gmail.com>
2025-02-21 14:59:12 +0700

    update

---
+++ lab1/src/main/java/lab01/bai3/Bai3.java
@@ -0,0 +1,14 @@
+package lab01.bai3;
+
+import java.util.Scanner;
+
+public class Bai3 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập số nguyên n, số thực m: ");
+        int n = sc.nextInt();
+        float m = sc.nextFloat();
+        System.out.println("Số nguyên n vừa nhập: " +n);
+        System.out.println("Số thực m vừa nhập: " +m);
+    }
+}
